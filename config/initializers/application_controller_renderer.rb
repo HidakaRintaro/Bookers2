fca585e3369c617e4578022dc3ed454722a7885d
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-07-12 18:11:06 +0900

    [Finish] 5-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0d62d0b517fb1cb3cacdf5e6f6bec907bb675f53a2624c72312e8bf2ece777611227e56f9da2ee491d7a1bb3319873177362a0aa19e249518d5f25d7b6044aeb'
